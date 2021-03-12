8a94ee69f582dc8bb5231701bc28465f4faf9131
Gilles Piou <gilles.piou@gmail.com>
2021-03-13 02:55:41 +0800

    Kick-start ftx-lending

---
+++ index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const cctx = require("ccxt");
+
+const ftx = new cctx.ftx({
+  apiKey: process.env.FTX_API_KEY,
+  secret: process.env.FTX_SECRET,
+});
+
+ftx
+  .privateGetSpotMarginLendingInfo()
+  .then(function ({ result }) {
+    const updatingOffers = result
+      .map(function ({ coin, lendable }) {
+        return ftx.privatePostSpotMarginOffers({
+          coin,
+          size: lendable,
+          rate: 1e-6,
+        });
+      });
+    return Promise.all(updatingOffers);
+  });
